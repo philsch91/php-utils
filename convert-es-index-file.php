283e0943c167a1e32423359d40ce343c84579756
Philipp Schunker <philipp.schunker@allianz.at>
2021-01-18 12:43:36 +0100

    updated convert-es-index-file.php

--- convert-es-index-file.php
+++ convert-es-index-file.php
@@ -1,16 +1,43 @@
 <?php
 
+
+
 print_r($argv);
 
-if (empty($argv[1])) {
+// shell
+if (!empty($argv) && empty($argv[1])) {
     echo("please provide a filename as argument");
     exit(1);
 }
 
-$filename = $argv[1];
-$json = file_get_contents($filename);
+if (!empty($argv[1])) {
+    $filename = $argv[1];
+    $json = file_get_contents($filename);
+}
+
+// webserver
+if (!empty($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
+    $json = file_get_contents("php://input");
+}
+
+if ($json == false || empty($json)) {
+    echo("Error: missing input");
+    exit(1);
+}
+
+if (!isset($json)) {
+    echo("Exception: missing JSON string");
+    exit(2);
+}
+
 //print_r($json);
 $dict = json_decode($json, true);
+
+if ($dict === null) {
+    echo("DeserializationError: invalid JSON string");
+    exit(3);
+}
+
 //print_r($dict);
 $hitsList = $dict["hits"]["hits"];
 $str = "";
@@ -23,7 +50,15 @@ foreach ($hitsList as $i => $hit) {
     $str .= $message."\n";
 }
 
-$filename .= ".log";
+if (!empty($filename)) {
+    // shell
+    $filename .= ".log";
+} else {
+    // webserver
+    $filename = time().".log";
+}
+
+echo("filename is ".$filename);
 
 if (!empty($argv[2])) {
     $filename = $argv[2];
@@ -34,7 +69,14 @@ if (file_exists($filename)) {
     $str = $s.$str;
 }
 
-file_put_contents($filename, $str);
+$bytes = file_put_contents($filename, $str);
+
+if ($bytes === false) {
+    echo("FileWriteException: could not write ".$filename);
+    exit(4);
+}
+
+echo($filename." was successfully written");
 
 exit(0);
 
